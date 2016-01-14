d96f0d219021be7e65e08dc395e112c9c12dcfd6
Ian Wong <ianmwong@cmail.carleton.ca>
2016-01-14 11:18:08 -0500

    root change

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'application#hello'
+  root 'blogposts#index'
 
+  get 'hello' => 'application#hello'
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160114155246) do
-
-  create_table "blogposts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "contents"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
